d7be007243bd2e82a50082c12e2e6d762b55a5ad
kitkat0225 <kit-kat.0225@outlook.com>
2020-11-13 13:49:05 -0800

    updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Get()
+  test() {
+    return 'This is a test';
   }
 }
